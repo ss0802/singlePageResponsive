64c917801f71b028f77d211432c534f672c8d042
Saurabh Singh <ss0802@gmail.com>
2015-11-06 09:54:03 +0530

    Single page responsive design

---
+++ ajaxSubmit.php
@@ -0,0 +1,24 @@
+
+
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$name = $_POST['message'];
+
+$header = "From:" . $email . "\r\n";
+$header .= "X-Mailer: PHP /" .phpversion() . "\r\n";
+$header .= "Mime Version: 1.0 \r\n";
+$header .= "Content Type: text/plain";
+
+$comment = "This message has been sent by " . $name . "\r\n";
+$comment .= "E-mail it's: ". $email . "\r\n";
+$comment .= 'He's message is: '$email . "\r\n";
+
+$for = "ss0802@gmail.com";
+$Subject = "Contact from website";
+
+mail($subject, utf8_decode($comment), $header);
+
+echo json_encode(array(
+	'Message' => sprintf("Your message has been sent %s", $name);
+));
